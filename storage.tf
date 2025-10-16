10f4acfdc4d8aedc6e49a871cd91941d3544904c
Raul Sebastian Carreno Reyes <administrador@MacBook-Pro-de-Administrador.local>
2025-10-16 16:38:01 -0500

    storage

--- storage.tf
+++ storage.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "cmtr_bucket" {
   bucket = "cmtr-d8cbjg27-bucket-1760650184"
 
   # Bloqueo de acceso público (recomendado en labs de seguridad)
-  acl    = "private"
+  acl = "private"
 
   tags = {
     Project = "cmtr-d8cbjg27"

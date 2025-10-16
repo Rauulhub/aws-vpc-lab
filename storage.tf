16e05d73d0c5094722c6f5091ad355bc3d041565
Raul Sebastian Carreno Reyes <administrador@MacBook-Pro-de-Administrador.local>
2025-10-16 16:54:47 -0500

    storage

--- storage.tf
+++ storage.tf
@@ -1,10 +1,13 @@
 resource "aws_s3_bucket" "cmtr_bucket" {
   bucket = "cmtr-d8cbjg27-bucket-1760650184"
 
-  # Bloqueo de acceso público (recomendado en labs de seguridad)
-  acl = "private"
-
   tags = {
     Project = "cmtr-d8cbjg27"
   }
+}
+
+# Asignamos el ACL con el nuevo recurso
+resource "aws_s3_bucket_acl" "cmtr_bucket_acl" {
+  bucket = aws_s3_bucket.cmtr_bucket.id
+  acl    = "private"
 }

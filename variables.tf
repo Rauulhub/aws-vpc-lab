b298206093ecfa02f0cd2e41c7843a79c8486cf6
Raul Sebastian Carreno Reyes <administrador@MacBook-Pro-de-Administrador.local>
2025-10-17 10:40:18 -0500

    :rocket: net sec

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "ssh_key" {
-  description = "Provides custom public SSH key."
-  type        = string
-  default     = ""
-}
-
 # opcionales (si quieres controlar la región o tipo de instancia)
 variable "region" {
   description = "AWS region"
@@ -11,8 +5,38 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
+variable "allowed_ip_range" {
+  description = "Lista de rangos IP permitidos (CIDR). Ej: [\"18.153.146.156/32\",\"203.0.113.25/32\"]"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "ID del VPC provisto por la plataforma"
   type        = string
-  default     = "t3.micro"
+}
+
+variable "public_subnet_id" {
+  description = "ID del subnet público provisto por la plataforma"
+  type        = string
+}
+
+variable "private_subnet_id" {
+  description = "ID del subnet privado provisto por la plataforma"
+  type        = string
+}
+
+variable "public_instance_id" {
+  description = "ID de la instancia pública (ej: i-0b18bb948942be3ac)"
+  type        = string
+}
+
+variable "private_instance_id" {
+  description = "ID de la instancia privada (ej: i-046aa15449aae1cb5)"
+  type        = string
+}
+
+variable "project" {
+  description = "Tag Project para aplicar a los security groups"
+  type        = string
+  default     = "cmtr-d8cbjg27"
 }

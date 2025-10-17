2a8c3ab963460685d06a8488f7b87bb558bfb8a9
Raul Sebastian Carreno Reyes <administrador@MacBook-Pro-de-Administrador.local>
2025-10-17 11:18:50 -0500

    :rocket: finally

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
-# opcionales (si quieres controlar la región o tipo de instancia)
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
+  type        = string
   default     = "10.10.0.0/16"
 }
 
 variable "public_subnets" {
-  description = "Map of public subnets"
+  description = "Map of public subnets with CIDR and AZ"
   type = map(object({
     cidr = string
     az   = string
   }))
   default = {
-    subnet_a = { cidr = "10.10.1.0/24", az = "us-east-1a" }
-    subnet_b = { cidr = "10.10.3.0/24", az = "us-east-1b" }
-    subnet_c = { cidr = "10.10.5.0/24", az = "us-east-1c" }
+    a = { cidr = "10.10.1.0/24", az = "us-east-1a" }
+    b = { cidr = "10.10.3.0/24", az = "us-east-1b" }
+    c = { cidr = "10.10.5.0/24", az = "us-east-1c" }
   }
+}
+
+variable "region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
 }

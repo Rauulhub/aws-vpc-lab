1bbafb961f9c4a50c30ae811d90d864ced7bb47d
Raul Sebastian Carreno Reyes <administrador@MacBook-Pro-de-Administrador.local>
2025-10-17 16:27:18 -0500

    :rocket: alb lab

--- variables.tf
+++ variables.tf
@@ -1,19 +1,87 @@
+# -------------------------
+# VARIABLES GLOBALES
+# -------------------------
+
 variable "region" {
-  description = "AWS region where resources will be created"
+  description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
-variable "project_id" {
-  description = "Project identifier for tagging"
+
+variable "project_name" {
+  description = "Project name used for tagging and resource naming"
   type        = string
 }
 
-variable "state_bucket" {
-  description = "S3 bucket name for remote Terraform state"
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
   type        = string
 }
 
-variable "state_key" {
-  description = "S3 key path for remote Terraform state file"
+variable "ssh_key_name" {
+  description = "SSH key pair name"
   type        = string
+}
+
+variable "instance_profile_name" {
+  description = "IAM instance profile name for EC2 instances"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "S3 bucket where instance startup script uploads file"
+  type        = string
+}
+
+# -------------------------
+# REDES Y SEGURIDAD
+# -------------------------
+
+variable "vpc_id" {
+  description = "ID of the VPC where resources will be deployed"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet IDs (for ALB)"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs (for Auto Scaling Group)"
+  type        = list(string)
+}
+
+variable "ec2_sg_id" {
+  description = "Security Group ID for EC2 SSH access"
+  type        = string
+}
+
+variable "http_sg_id" {
+  description = "Security Group ID for HTTP access to EC2 instances"
+  type        = string
+}
+
+variable "alb_sg_id" {
+  description = "Security Group ID for the Application Load Balancer"
+  type        = string
+}
+
+# -------------------------
+# AUTO SCALING CONFIG
+# -------------------------
+
+variable "asg_min_size" {
+  description = "Minimum number of EC2 instances in Auto Scaling Group"
+  type        = number
+}
+
+variable "asg_desired_size" {
+  description = "Desired number of EC2 instances in Auto Scaling Group"
+  type        = number
+}
+
+variable "asg_max_size" {
+  description = "Maximum number of EC2 instances in Auto Scaling Group"
+  type        = number
 }

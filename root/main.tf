f54bf1c8af20e393b46a88e54063f2c94a20fd68
Raul Sebastian Carreno Reyes <administrador@MacBook-Pro-de-Administrador.local>
2025-10-23 13:20:13 -0500

    :rocket: task 14

--- root/main.tf
+++ root/main.tf
@@ -1,16 +1,7 @@
 provider "aws" {
   region = var.region
 }
-terraform {
-  required_version = ">=1.5.7"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 module "network" {
   source              = "../modules/network"

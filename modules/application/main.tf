e172fc5ede5eab271dabe06c275ed488280b29af
Raul Sebastian Carreno Reyes <administrador@MacBook-Pro-de-Administrador.local>
2025-10-23 13:58:51 -0500

    test

--- modules/application/main.tf
+++ modules/application/main.tf
@@ -87,3 +87,7 @@ resource "aws_autoscaling_group" "asg" {
   health_check_type         = "ELB"
   health_check_grace_period = 300
 }
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  lb_target_group_arn    = aws_lb_target_group.app_tg.arn
+}
